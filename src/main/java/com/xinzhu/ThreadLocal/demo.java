d1d166b37c704225dd6f176eb4d4eaaa5f6806f4
xinzhu <1148573442@qq.com>
2021-10-11 15:06:25 +0800

    demo测试提交

---
+++ src/main/java/com/xinzhu/ThreadLocal/demo.java
@@ -0,0 +1,10 @@
+package com.xinzhu.ThreadLocal;
+
+/**
+ * Create By GuoFZ on 2021/10/11
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("qqq");
+    }
+}
