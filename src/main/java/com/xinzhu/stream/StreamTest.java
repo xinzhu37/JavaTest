8e995837bf37d20c3abe5f8d3a8df3c5adb670f6
xinzhu37 <76461030+xinzhu37@users.noreply.github.com>
2022-01-11 10:55:52 +0800

    Update StreamTest.java
    远程提交4444444444444444

--- src/main/java/com/xinzhu/stream/StreamTest.java
+++ src/main/java/com/xinzhu/stream/StreamTest.java
@@ -90,6 +90,7 @@ public class StreamTest {
         System.out.println("1111111111111");
         System.out.println("2222222222222");
         System.out.println("3333333333333");
+        System.out.println("4444444444444");
 
     }
 }
